b34a23f18da07cb86b90c4c791d33ec3cd57f0a3
Ben <benjamin_clayman@alumni.brown.edu>
2013-02-16 22:17:11 -0500

    added routes

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,24 @@
+class ResumesController < ApplicationController
+  def index
+    @resumes = Resume.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
